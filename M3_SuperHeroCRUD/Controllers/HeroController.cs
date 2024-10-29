4c9bbe9a3995657ae1a21c72b74fb3dee055dcb3
TothDavid <130594869+Flitpod@users.noreply.github.com>
2024-10-29 19:55:22 +0200

    Controllers - SuperHero controller added

---
+++ M3_SuperHeroCRUD/Controllers/HeroController.cs
@@ -0,0 +1,35 @@
+﻿using M3_SuperHeroCRUD.Data;
+using M3_SuperHeroCRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace M3_SuperHeroCRUD.Controllers
+{
+    public class HeroController : Controller
+    {
+        ISuperHeroRepository repository;
+
+        public HeroController(ISuperHeroRepository repository)
+        {
+            this.repository = repository;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(this.repository.Read());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(SuperHero superHero)
+        {
+            this.repository.Create(superHero);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
