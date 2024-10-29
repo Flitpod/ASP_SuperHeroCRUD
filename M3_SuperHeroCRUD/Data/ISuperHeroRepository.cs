bd59827d81d79a438828bd83cc46e73eae47ec1f
TothDavid <130594869+Flitpod@users.noreply.github.com>
2024-10-29 17:49:07 +0200

    Data - SuperHeroRepository added with interface and configured with IoC

---
+++ M3_SuperHeroCRUD/Data/ISuperHeroRepository.cs
@@ -0,0 +1,13 @@
+﻿using M3_SuperHeroCRUD.Models;
+
+namespace M3_SuperHeroCRUD.Data
+{
+    public interface ISuperHeroRepository
+    {
+        void Create(SuperHero superHero);
+        bool Delete(string name);
+        IEnumerable<SuperHero> Read();
+        SuperHero Read(string name);
+        void Update(SuperHero superHero);
+    }
+}
--- M3_SuperHeroCRUD/Program.cs
+++ M3_SuperHeroCRUD/Program.cs
@@ -5,9 +5,16 @@ namespace M3_SuperHeroCRUD
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            
+            builder.Services.AddControllersWithViews();
+            
             var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World!");
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapControllers();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
