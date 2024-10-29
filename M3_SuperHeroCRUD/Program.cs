b58995379cd65617ca7b1135972f8868ea6c2a3b
TothDavid <130594869+Flitpod@users.noreply.github.com>
2024-10-29 17:49:27 +0200

    Revert "Data - SuperHeroRepository added with interface and configured with IoC"
    This reverts commit bd59827d81d79a438828bd83cc46e73eae47ec1f.

--- M3_SuperHeroCRUD/Program.cs
+++ M3_SuperHeroCRUD/Program.cs
@@ -5,16 +5,9 @@ namespace M3_SuperHeroCRUD
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            
-            builder.Services.AddControllersWithViews();
-            
             var app = builder.Build();
-            app.UseStaticFiles();
-            app.UseRouting();
-            app.MapControllers();
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.MapGet("/", () => "Hello World!");
 
             app.Run();
         }
