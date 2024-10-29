dc361c05f574f064041912e955bc1d593c154ece
TothDavid <130594869+Flitpod@users.noreply.github.com>
2024-10-29 17:29:51 +0200

    Models - SuperHero model added with validation attributes

---
+++ M3_SuperHeroCRUD/Models/SuperHero.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace M3_SuperHeroCRUD.Models
+{
+    public enum HeroSide
+    {
+        Civil = 0, Good, Bad 
+    }
+
+    public class SuperHero
+    {
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1,10)]
+        public int Power { get; set; }
+    
+        public bool IsAlien { get; set; }
+
+        [Required]
+        public HeroSide HeroSide { get; set; }
+    }
+}
