03315b7ee29887b584350f939508e0ac6611cd05
TothDavid <130594869+Flitpod@users.noreply.github.com>
2024-11-21 19:35:19 +0200

    Console app shallow client for api test added

---
+++ M3_SuperHeroClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using M3_SuperHeroClient;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text.Json.Serialization;
+
+
+var jsonData = new HttpClient().GetStringAsync("http://localhost:5253/api").Result;
+var superHeros = JsonConvert.DeserializeObject<IEnumerable<SuperHero>>(jsonData);
+foreach (var item in superHeros)
+{
+    Console.WriteLine(item.Name + " " + item.Power + " " + item.HeroSide);
+}
+
+Console.ReadKey();
---
+++ M3_SuperHeroClient/SuperHero.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace M3_SuperHeroClient
+{
+    public enum HeroSide
+    {
+        [Display(Name = "Civil")]
+        Civil = 0,
+
+        [Display(Name = "Good")]
+        Good = 1,
+
+        [Display(Name = "Bad")]
+        Bad = 2
+    }
+
+    public class SuperHero
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Maximum hossz 50 karakter lehet!")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1,10, ErrorMessage = "Az erő minimum 1 és maximum 10 értékű lehet")]
+        public int Power { get; set; }
+        
+        public bool IsAlien { get; set; }
+
+        [Required]
+        public HeroSide HeroSide { get; set; }
+
+        public SuperHero()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
